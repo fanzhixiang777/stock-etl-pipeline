5dff361accdbc83fda5cab4c0fee38eb2bd7f9a9
fanzhixiang777 <fanzhixiang683@gmail.com>
2026-07-10 19:38:02 +0800

    Update god.py

--- god.py
+++ god.py
@@ -3,14 +3,14 @@ import pandas as pd
 import sqlite3
 from config import API_KEY
 
-syboml = []
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={syboml}&apikey={API_KEY}"
+symbol = []
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}"
 response = requests.get(url)
 data = response.json()
 daily = data["Time Series (Daily)"]
 df = pd.DataFrame(daily).T
 df = df.apply(pd.to_numeric)
 conn = sqlite3.connect('stocks.db')
-df.to_sql(f'{sybmol}_daily', conn, if_exists='replace')
-check = pd.read_sql(f'SELECT * FROM {sybmol}_daily', conn)
+df.to_sql(f'{symbol}_daily', conn, if_exists='replace')
+check = pd.read_sql(f'SELECT * FROM {symbol}_daily', conn)
 print(check)
